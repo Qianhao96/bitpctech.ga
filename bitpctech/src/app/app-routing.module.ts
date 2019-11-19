c85c1c725f412663f1e86979516de08309f102f1
CunjunYin <cunjunyin@outlook.com>
2019-11-19 20:12:52 +0800

    complete footer

--- bitpctech/src/app/app-routing.module.ts
+++ bitpctech/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginAndRegistrationComponent } from './user/login-and-registration/login-and-registration.component';
+import { AboutComponent } from './sharedComponent/about/about.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/', pathMatch: 'full' },
-  {
-    path: 'user/login', component: LoginAndRegistrationComponent},
+  { path: 'user/login', component: LoginAndRegistrationComponent },
+  { path: 'sharedComponent/about', component: AboutComponent}
 ];
 @NgModule({
   declarations: [],
